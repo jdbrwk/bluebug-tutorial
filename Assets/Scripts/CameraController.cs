57ac29994c61750c2cba93b9f74a96ff49c72142
Jerzy@Mac <jerzy.dabrowka@o2.pl>
2018-07-04 19:21:14 +0200

    nowy projekt

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    
+    public GameObject player;
+
+    private Vector3 offset;
+
+    void Start ()
+
+    {
+        offset = transform.position - player.transform.position;
+
+    }
+
+    // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
+
+    void LateUpdate ()
+
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
