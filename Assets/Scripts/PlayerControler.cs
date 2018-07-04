3bc5d5c219a71de8fa5cd12f68c72c885d0728b0
Jerzy@PC <jerzy.dabrowka@o2.pl>
2018-07-04 22:31:04 +0200

    zakonczona gra
    dodanie kolizji, zbierania punktow, wyswietlenie tekstu o wygranej oraz binarka

--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -1,15 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerControler : MonoBehaviour {
 
     public float Speed;
+	public Text countText;
+	public Text winText;
+
     private Rigidbody2D rb2d;
+	private int count;
 
     void Start ()
     {
         rb2d = GetComponent<Rigidbody2D>();
+		count = 0;
+		winText.text ="";
+		SetCountText ();
+
     }
 
 
@@ -20,7 +29,28 @@ public class PlayerControler : MonoBehaviour {
         float moveVertical = Input.GetAxis ("Vertical");
         Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
         rb2d.AddForce(movement*Speed);
+	}
 
+
+    //OnTriggerEnter2D is called whenever this object overlaps with a trigger collider.
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        //Check the provided Collider2D parameter other to see if it is tagged "PickUp", if it is...
+        if (other.gameObject.CompareTag("PickUp"))
+                {
+                     other.gameObject.SetActive(false);
+					 count = count + 1;
+					 SetCountText ();
+                }
+    }
+
+	void SetCountText () 
+	{
+	 countText.text = "Punkty: " + count.ToString ();
+	 if (count >= 13) 
+	 {
+		winText.text = "Jesteś zwyciężcą!";
+	 }
 	}
 
 }
